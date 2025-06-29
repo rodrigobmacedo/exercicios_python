79e38bef8aa08815079f16e643f327df300b6d95
Rodrigo Brunetti Macedo <rbmacedo2005@hotmail.com>
2025-06-29 03:08:34 -0300

    Atualização da pasta 29.06.25

---
+++ exercício_intervalo_de_numeros.py
@@ -0,0 +1,11 @@
+"""
+For + Range
+range -> range(start, stop, step)
+
+start = início
+stop = número máximo
+step = quantidade de saltos
+"""
+numeros = range(0, 80, 4)
+for numero in numeros:
+    print(numero)
---
+++ jogo_da_palavra_secreta.py
@@ -0,0 +1,41 @@
+tentativas = 5
+palavra = input("Digite a palavra para secreta")
+palavra_secreta = palavra.upper()
+palavra_escondida = "_"*len(palavra_secreta)
+lista_palavra = list(palavra_escondida)
+tamanho_palavra = len(palavra_secreta)
+letras_digitadas = ""
+cont_acerto = 0;
+i = 0
+ganhou = False
+print("\n\n INICIANDO O JOGO! \n\n")
+while tentativas != 0 and ganhou == False:
+    cont_repetida = 0
+    lista_palavra = list(lista_palavra)
+    letra = input("\nDigite uma letra para procurar: ")
+    if letra in letras_digitadas:
+        print("Esta letra ja foi digitada")
+        cont_repetida = 1
+    else:    
+        letras_digitadas += letra+"-"
+        letra_procurada = letra.upper()
+        for i in range(tamanho_palavra):
+            if letra_procurada == palavra_secreta[i]:
+                lista_palavra[i] = letra_procurada
+                cont_acerto = 1
+            elif letra_procurada != [i] and cont_acerto == 0:
+                cont_erro = 1
+                cont_acerto = 0
+            if "_" not in lista_palavra :
+                lista_palavra = "".join(lista_palavra)
+                print(f"PARABÉNS, VOCÊ VENCEU\na palavra secreta era: {lista_palavra}")
+                ganhou = True   
+    if cont_acerto == 0 and cont_repetida != 1:
+        tentativas -= 1        
+        print("\nVOCÊ ERROU A LETRA\n")
+    lista_palavra = "".join(lista_palavra)
+    if ganhou == False and tentativas >0:
+        print(f"VOCÊ AINDA TEM {tentativas} CHANCES\n{lista_palavra}")
+        print(letras_digitadas)
+    if tentativas == 0:
+        print(f"VOCÊ PERDEU\nA palavra secreta era {palavra_secreta}")   
